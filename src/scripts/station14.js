fd4863a15573f7027373ae451689742ab087ad1c
misosiru0205 <misosiru0205@gmail.com>
2023-11-25 18:05:53 +0900

    git commit -n

---
+++ src/scripts/station14.js
@@ -0,0 +1,5 @@
+const btn1 = document.getElementById('1');
+
+btn1.addEventListener('click',()=>{
+    alert('ECMAScript');
+});
---
+++ src/scripts/station15.js
@@ -0,0 +1,3 @@
+function displayList(){
+    document.getElementById("fruits").innerHTML = "<ul><li>リンゴ</li><li>イチゴ</li><li>ブドウ</li> </ul>";
+};
